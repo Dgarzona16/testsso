a4cab9802a0ed709b237704078cbc3623866c8d6
Dgarzona <Daniel@t7g2l.onmicrosoft.com>
2024-08-07 15:54:02 -0600

    change redirect

--- src/config/authconfig.js
+++ src/config/authconfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
         clientId: '8f86647c-14c2-4fd4-9eb7-3cf255c3bcbc',
         authority: 'https://login.microsoftonline.com/common',
-        redirectUri: 'https://yellow-hill-0ca47791e.5.azurestaticapps.net/', 
+        redirectUri: 'https://testsso-zeta.vercel.app/', 
     },
     cache: {
         cacheLocation: 'sessionStorage',
